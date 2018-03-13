3af5b9d61f27be0e95308305505278c281c4ff30
gongzza <gz.us.to@gmail.com>
2018-03-13 23:08:14 +0900

    :fire: Remove utils
    컴파일 실패 테스트가 평균 2초씩 너무 오래 걸려서 제거함

--- test/basic_types.test.ts
+++ test/basic_types.test.ts
@@ -1,18 +1,16 @@
-import {createProgram, getEmitDiagnostics} from './utils'
-
 describe('Basic Types', () => {
-  describe('Boolean', () => {
-    test('typeof', () => {
-      let isDone: boolean = false
-      expect(typeof isDone).toBe('boolean')
-    })
-    test('type not assignable', () => {
-      const program = createProgram(`
-        let isDone: boolean = false
-        isDone = 1
-      `)
-      const diagnostics = getEmitDiagnostics(program)
-      expect(diagnostics.pop().messageText).toEqual(`Type '1' is not assignable to type 'boolean'.`)
-    })
+  test('Boolean', () => {
+    let isDone: boolean = false
+    expect(typeof isDone).toBe('boolean')
+    expect(isDone).toBe(false)
+
+    isDone = true
+    expect(isDone).toBe(true)
+
+    // isDone = 1
+    // Type '1' is not assignable to type 'boolean'.
+
+    // isDone = 'str'
+    // Type '"str"' is not assignable to type 'booelan'.
   })
 })
